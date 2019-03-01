3465c87ff1f65f32bcfd95febce89fb97fbeb418
Muhammad Wisnu <inudola@users.noreply.github.com>
2019-03-01 10:41:01 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            REAK!!!!!!!
+            TUTUTUTUTUUTUT
           </a>
         </header>
       </div>
