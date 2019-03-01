4862f7ba8559851620e82047910b27479185ffa9
Muhammad Wisnu <inudola@users.noreply.github.com>
2019-03-01 10:41:40 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            TUTUTUTUTUUTUT
+            RADITSAN
           </a>
         </header>
       </div>
